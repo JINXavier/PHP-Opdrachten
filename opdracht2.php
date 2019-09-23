b71b31603761dd05a54a209133ae3d0afeae191e
Christopher <cxpop@outlook.com>
2019-09-23 09:03:07 +0200

    opdracht2 toegevoegd.

---
+++ opdracht2.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Christopher
+ * Date: 9/23/2019
+ * Time: 8:58 AM
+ */
+
+$boodschap ="aardappel";
+
+echo "Ik "." moet "." vandaag ".$boodschap."en "."kopen";
