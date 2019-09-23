5bf0b72d096f6517aecbe6ef5f82850b6925492c
Christopher <cxpop@outlook.com>
2019-09-23 09:57:38 +0200

    opdracht 3 (zonder bonus)

---
+++ opdracht3.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Christopher
+ * Date: 9/23/2019
+ * Time: 9:08 AM
+ */
+
+$boodschappen = ["aardappelen","aardbeien","3pakken melk","yoghurt"];
+
+foreach($boodschappen as $boodschap) {
+    echo "<li>".$boodschap."</li>";
+}
