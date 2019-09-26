2f99ea46cedfa3e6e2e400bc93c9c9923a4461de
Christopher <cxpop@outlook.com>
2019-09-26 10:45:35 +0200

    opdracht 6

---
+++ opdracht6.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Christopher
+ * Date: 9/25/2019
+ * Time: 8:41 AM
+ */
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "boodschappen";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Opdracht 6</title>
+</head>
+<body>
+<?php
+
+
+?>
+</body>
+</html>
